d22bc73f0085615240dca9efc7743ffe6740f61d
Matt Ross <amsross@gmail.com>
2018-03-08 14:09:06 -0500

    chore: clean up unused example

--- examples/traverse.js
+++ examples/traverse.js
@@ -22,32 +22,6 @@ h.of({ home: 1, away: 2 })
   .through(traverse(h.of, getTeam))
   .sequence()
   .reduce1((x, y) => Object.assign({}, x, y))
-  .tap(xs => console.timeEnd('traverse', xs))
+  .tap(() => console.timeEnd('traverse'))
   .tap(log('traverse'))
   .each(xs => deepEqual(xs, { home: 1, away: 2 }))
-
-// const toUpperCase = x => x.toUpperCase()
-// const split = str => x => x.split(str)
-// const reverse = x => x.reverse()
-// const join = str => x => x.join(str)
-
-// h.of(f => g => h => i => x => f(g(h(i(x)))))
-//   .through(ap(h.of(toUpperCase)))
-//   .through(ap(h.of(join(''))))
-//   .through(ap(h.of(reverse)))
-//   .through(ap(h.of(split(''))))
-//   .through(ap(h([ 'ih', 'eyb' ])))
-//   .collect()
-//   .tap(log('greetings'))
-//   .each(xs => deepEqual(xs, [ 'HI', 'BYE' ]))
-
-// const add = y => x => x + y
-// const times = y => x => x * y
-
-// h.of(f => g => x => f(g(x)))
-//   .through(ap(h.of((add(1)))))
-//   .through(ap(h.of(times(4))))
-//   .through(ap(h([ 1, 3 ])))
-//   .collect()
-//   .tap(log('math'))
-//   .each(xs => deepEqual(xs, [ 5, 13 ]))
